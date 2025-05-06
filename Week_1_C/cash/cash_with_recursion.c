c9d4691f37887432f7a0904c89fbd14d764ed06b
Vitor Betmann <143951155+vitorbetmann@users.noreply.github.com>
2025-05-06 00:56:33 -0400

    Update cash_with_recursion.c

--- Week_1_C/cash/cash_with_recursion.c
+++ Week_1_C/cash/cash_with_recursion.c
@@ -1,29 +1,41 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int coins_amount(int change, int values[], int index);
+// prototypes
+int get_change();
+int coin_amount(int change, int index);
+
+// variables
+int coins[] = {25, 10, 5, 1};
 
 int main(void)
 {
-    int change;
+    // prompt user for change amount
+    int change = get_change();
+    // calculate minimum amount of coins to pay change
+    int amount = coin_amount(change, 0);
+    // print amount
+    printf("%d\n", amount);
+}
+
+int get_change()
+{
+    int change = -1; // set it to -1 because if just declared, it contains garbage value
     do
     {
         change = get_int("Change owed: ");
     }
-    while (change <= 0);
-
-    int values[] = {25, 10, 5, 1};
-    printf("%i\n", coins_amount(change, values, 0));
+    while (change < 0);
+    return change;
 }
 
-int coins_amount(int change, int values[], int index)
-{
-    if (change == 0)
-    {
+int coin_amount(int change, int index){
+    if (change == 0){
         return 0;
     }
 
-    int max_coin_amount = change / values[index];
-    int remainder = change % values[index];
-    return max_coin_amount + coins_amount(remainder, values, ++index);
-}
+    int value = coins[index];
+    int amount = change / value;
+    change = change % value;
+    return amount + coin_amount(change, ++index);
+ }
